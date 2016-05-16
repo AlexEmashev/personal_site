98f07e4ecd9678195e2eb2216aa5b31f8d5e4be0
Alexander Emashev <alex.emashev@gmail.com>
2016-05-16 22:02:08 +0300

    Change using technologies to gulp, bower, nunjucks, autoprefixer.

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    batch = require('gulp-batch'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    nunjucksRender = require('gulp-nunjucks-render'),
+    watch = require('gulp-watch'),
+    del = require('del'),
+    nunjucksDefaults = {path: 'src/templates/'},
+    plumber = require('gulp-plumber'),
+    beepbeep = require('beepbeep');
+
+// Reaction in case of error
+function errorlog(err) {
+  beepbeep();
+	console.error(err.message);
+	this.emit('end');
+}
+
+// ///////////////////////////////////////////////////
+// // Watch Task
+// ///////////////////////////////////////////////////
+// gulp.task('watch', ['scss', 'html', 'browser-sync']);
+
+gulp.task('default', ['compile']);
+
+///////////////////////////////////////////////////
+// SCSS compile
+///////////////////////////////////////////////////
+gulp.task('scss', function() {
+  return sass('src/scss/style.scss')
+  .on('error', sass.logError)
+  .pipe(autoprefixer('last 2 versions'))
+  .pipe(gulp.dest('dist/css/'));
+  //.pipe(reload({stream:true}));
+});
+
+gulp.task('compile', function() {
+  watch('src/scss/**/*.scss',function () {
+    gulp.start('scss');
+  });
+  watch('src/templates/**/*.+(html|njk|nunjucks)', function () {
+    gulp.start('html');
+  });
+});
+
+///////////////////////////////////////////////////
+// Html compile
+///////////////////////////////////////////////////
+gulp.task('html', function() {
+  return gulp.src('src/templates/pages/**/*.+(html|njk|nunjucks)')
+  .pipe(plumber())
+  .pipe(nunjucksRender(nunjucksDefaults))
+  .on('error', errorlog)
+  .pipe(gulp.dest('dist/'));
+  //.pipe(reload({stream:true}));
+});
+
+///////////////////////////////////////////////////
+// Browser Sync
+///////////////////////////////////////////////////
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  });
+  watch('./dist/**/*')
+  .pipe(reload({stream:true}));
+});
